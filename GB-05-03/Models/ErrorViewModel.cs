f9814725d1bc611bca2f6d865dbd5d12c602bad2
Artem <a.khimin@yandex.ru>
2022-02-06 00:25:22 +0300

    Добавьте файлы проекта.

---
+++ GB-05-03/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GB_05_03.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
