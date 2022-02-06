f4c816adf551a477639d2b10569bd0cc74c9bfa3
Artem <a.khimin@yandex.ru>
2022-02-06 19:46:55 +0300

    Task3

---
+++ GB-05-03/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace GB_05_03.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Company { get; set; }
+        public int Age { get; set; }
+
+
+    }
+}
