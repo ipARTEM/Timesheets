cdaff7a86db8807bab24d66bcf996ba2787ae4ba
Artem <a.khimin@yandex.ru>
2022-02-19 16:40:56 +0300

    Перекинул data в db

--- Timesheets.API/Controllers/PeopleController.cs
+++ Timesheets.API/Controllers/PeopleController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Timesheets.BL.Models;
+using Timesheets.DB;
 
 namespace Timesheets.API.Controllers
 {
--- Timesheets.BL/Models/IPeople.cs
+++ Timesheets.BL/Models/IPeople.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 
+using Timesheets.DB;
+
 namespace Timesheets.BL.Models
 {
     public interface IPeople
